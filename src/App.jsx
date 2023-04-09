fc49c1a951ea48153027445647a3eecdb7b8015a
Ivan Telleria <ivantelleria96@gmail.com>
2023-04-09 10:41:27 +0200

    Created custom ook to separate logic from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,10 @@
-import { useState } from 'react'
+import { useGame } from './hooks/useGame'
 import { Square } from './components/Square'
 import { TURNS } from './constanst'
-import { checkWinnerFrom, checkEndGame } from './logic/board'
 import WinnerModal from './components/WinnerModal'
-import confetti from 'canvas-confetti'
 
 function App () {
-  const [board, setBoard] = useState(() => {
-    const boardFromStorage = window.localStorage.getItem('board')
-
-    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
-  })
-  const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn')
-
-    return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.x
-  })
-  const [winner, setWinner] = useState(null)
-
-  const updateBoard = (index) => {
-    if (board[index] || winner) return
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setBoard(newBoard)
-
-    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
-    setTurn(newTurn)
-
-    window.localStorage.setItem('board', JSON.stringify(newBoard))
-    window.localStorage.setItem('turn', JSON.stringify(newTurn))
-
-    const newWinner = checkWinnerFrom(newBoard)
-
-    if (newWinner) {
-      confetti()
-      setWinner(newWinner)
-    } else if (checkEndGame(newBoard)) {
-      setWinner(false)
-    }
-  }
-
-  const resetGame = () => {
-    setBoard(Array(9).fill(null))
-    setTurn(TURNS.x)
-    setWinner(null)
-
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
-  }
+  const { resetGame, updateBoard, turn, winner, board } = useGame()
 
   return (
     <main className='board'>
---
+++ src/hooks/useGame.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { TURNS } from '../constanst'
+import { checkWinnerFrom, checkEndGame } from '../logic/board'
+import confetti from 'canvas-confetti'
+
+export const useGame = () => {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+
+    return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.x
+  })
+  const [winner, setWinner] = useState(null)
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
+    setTurn(newTurn)
+
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', JSON.stringify(newTurn))
+
+    const newWinner = checkWinnerFrom(newBoard)
+
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.x)
+    setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+  }
+
+  return { updateBoard, resetGame }
+}
